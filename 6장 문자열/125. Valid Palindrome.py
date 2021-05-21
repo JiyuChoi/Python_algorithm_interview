971896b1034ca8ccb4341dd8a828d08bb954ba54
choijiyu <jiyou126@naver.com>
2021-05-21 22:29:55 +0900

    문자열 정규식 풀이 추가

--- 6장 문자열/125. Valid Palindrome.py
+++ 6장 문자열/125. Valid Palindrome.py
@@ -7,3 +7,11 @@ def isPalindrome(s):
         return strs[::-1] == strs
 
 print(isPalindrome("race a car"))
+
+
+# 정규식 및 슬라이싱 사용 풀이 (가장 빠름)
+import re
+        s = s.lower()
+        # 정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+        return s == s[::-1]
