51a8e340a52ecfa5d0a7ebdb0ef7e3a36f5c6195
choijiyu <jiyou126@naver.com>
2021-05-23 19:00:03 +0900

    문자열 뒤집기 투포인터 풀이 추가

--- 6장 문자열/344. Reverse String.py
+++ 6장 문자열/344. Reverse String.py
@@ -1,3 +1,12 @@
+# Pythonic way (이게 더 빠름)
 def reverseString(s):
-    s.reverse()
+    s.reverse() # 리스트에만 제공
     # s = s[::-1]
+
+# 투 포인터로 구현
+def reverseString(s):
+    left, right = 0, len(s)-1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
