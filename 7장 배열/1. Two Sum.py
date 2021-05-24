10f8c3841afd82fa77b63d5c2e5c51c25a3958d9
choijiyu <jiyou126@naver.com>
2021-05-25 00:40:33 +0900

    두 수의 풀이 추가

--- 7장 배열/1. Two Sum.py
+++ 7장 배열/1. Two Sum.py
@@ -5,4 +5,22 @@ def twoSum(nums, target):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
+
+# in을 이용한 탐색
+def twoSum(nums, target):
+    for i, n in enumerate(nums):
+        if target - n in nums[i+1:]:
+            return [i, nums[i+1:].index(target-n)+(i+1)]
+
+
+# 첫번째 수를 뺀 결과 키 조회
+def twoSum(nums, target):
+    nums_map = {}
+    for i, n in enumerate(nums):
+        nums_map[n] = i
+
+    for i, n in enumerate(nums):
+        if target - n in nums_map and i != nums_map[target-n]:
+            return [i, nums_map[target-n]]
+
 print(twoSum([2, 7, 11, 15], 9))
